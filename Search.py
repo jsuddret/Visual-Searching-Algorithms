12d65286ea57643c5c7ef02b379f1ce5a855f53e
Jake Suddreth <58892456+jsuddret@users.noreply.github.com>
2020-04-25 01:05:09 -0700

    Add files via upload

--- Search.py
+++ Search.py
@@ -5,6 +5,13 @@ import random
 import time
 
 
+def search_complete():
+    for identification_number in id_list:
+        canvas.itemconfig(id_list[identification_number], fill='blue')
+        iterations_label.config(text='Iterations: ' + str(iterations))
+
+
+
 def linear_search(values, search_for):
     global iterations
     iterations = 0
@@ -19,24 +26,30 @@ def linear_search(values, search_for):
         else:
             search_at += 1
         time.sleep(0.01)
+    search_complete()
 
 
-def interpolation_search(values, search_for):
+def binary_search(values, search_for):
     global iterations
     iterations = 0
-    index_zero = 0
-    index_n = (len(values) - 1)
-    while index_zero <= index_n and values[index_zero] <= search_for <= values[index_n]:
+    size = len(values)
+    left = 0
+    right = size - 1
+    while left <= right:
         iterations += 1
-        canvas.itemconfig(id_list[index_zero], fill='blue')
-        canvas.update()
-        index_middle = index_zero + int(((float(index_n - index_zero) / (values[index_n] - values[index_zero]))
-                                         * (search_for - values[index_zero])))
-        if values[index_middle] == search_for:
-            return
-        if values[index_middle] < search_for:
-            index_zero = index_middle + 1
-    return
+        mid = (left + right) // 2
+        if values[mid] < search_for:
+            canvas.itemconfig(id_list[left], fill='blue')
+            canvas.update()
+            time.sleep(0.01)
+            left = mid + 1
+        elif values[mid] > search_for:
+            canvas.itemconfig(id_list[right], fill='blue')
+            canvas.update()
+            time.sleep(0.01)
+            right = mid - 1
+        else:
+            search_complete()
 
 
 def circle(x, y, interval):
@@ -49,8 +62,7 @@ def circle(x, y, interval):
 
 
 def generate_visual(prompt):
-    global search_for_index
-    search_for_index = 0
+    global search_for_value
     id_list.clear()
     canvas.delete('all')
     try:
@@ -63,14 +75,13 @@ def generate_visual(prompt):
         for i in range(user_value):
             circle(x, y, spacing_interval)
             spacing_interval += 1
-            if spacing_interval == 38:
+            if spacing_interval == 70:
                 y += spacing
                 spacing_interval = 0
         search_for_value = id_list[search_for_index]
         canvas.itemconfig(search_for_value, fill='blue')
     except ValueError:
         prompt.configure(fg='red')
-    print(id_list)
 
 
 window = Tk()
@@ -80,27 +91,28 @@ win_h = 1044
 canvas = Canvas(window, width=win_w, height=win_h, bg='black')
 canvas.pack()
 
-user_prompt = Label(window, text='Please enter a number: 0 < n < 1216', bg='black', fg='white')
+user_prompt = Label(window, text='Please enter a number: 0 < n < 4200', bg='black', fg='white')
 user_prompt.place(x=50, y=65)
 user_input = Text(window, width=25, height=1)
 user_input.place(x=50, y=100)
 generate = Button(window, text='Generate', bg='black', fg='white', command=lambda: generate_visual(user_prompt))
 generate.place(x=50, y=135)
 
-spacing = 30
-diameter = 15
+spacing = 16
+diameter = 8
 
 object_id = 0
 id_list = []
 total = 0
 
-search_for_index = 0
 search_for_value = 0
 iterations = 0
 
+iterations_label = Label(window, text='Iterations: ' + str(iterations), bg='black', fg='white')
+iterations_label.place(x=720, y=25)
 linear_search_button = Button(window, text='Linear Search', command=lambda: linear_search(id_list, search_for_value))
-linear_search_button.place(x=50, y=205)
-interpolation_search_button = Button(window, text='Interpolation Search', command=lambda: interpolation_search(id_list, search_for_value))
-interpolation_search_button.place(x=50, y=240)
+linear_search_button.place(x=50, y=175)
+binary_search_button = Button(window, text='Binary Search', command=lambda: binary_search(id_list, search_for_value))
+binary_search_button.place(x=50, y=205)
 
 window.mainloop()
